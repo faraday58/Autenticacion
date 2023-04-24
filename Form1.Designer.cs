908a64f8d09f97bd2ce9a6d1d8a7a3b917d20e51
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-04-24 12:49:17 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Autenticacion
+{
+    partial class FormEntrada
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbPassword = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // txtbUsuario
+            // 
+            this.txtbUsuario.Location = new System.Drawing.Point(181, 72);
+            this.txtbUsuario.Name = "txtbUsuario";
+            this.txtbUsuario.Size = new System.Drawing.Size(227, 20);
+            this.txtbUsuario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtbPassword
+            // 
+            this.txtbPassword.Location = new System.Drawing.Point(181, 119);
+            this.txtbPassword.Name = "txtbPassword";
+            this.txtbPassword.PasswordChar = '*';
+            this.txtbPassword.Size = new System.Drawing.Size(227, 20);
+            this.txtbPassword.TabIndex = 1;
+            this.txtbPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbPassword_KeyPress);
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(181, 186);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(91, 34);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(306, 186);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(91, 34);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FormEntrada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 297);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txtbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbUsuario);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEntrada";
+            this.Text = "Entrada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbPassword;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Usuarios_G2_2023_II;
+using System.Windows.Forms;
+
+namespace Autenticacion
+{
+    public partial class FormEntrada : Form
+    {
+        public FormEntrada()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            /*Persona persona = new Persona("rafael","rafael123");
+            
+            if(txtbUsuario.Text == persona.Usuario && txtbPassword.Text== persona.Password )
+            {
+                MessageBox.Show("Autenticación exitosa");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }*/
+
+            GeneraUsuario generaUsuario = new GeneraUsuario();
+            generaUsuario.Generar();
+            Persona persona = (Persona)generaUsuario.usuarios[0];
+
+
+            if(txtbUsuario.Text == persona.Usuario && txtbPassword.Text == persona.Password    )
+            {
+                MessageBox.Show("Autenticación exitosa");
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }
+
+
+        }
+
+        private void txtbPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ( e.KeyChar == (char)Keys.Enter  )
+            {
+
+                GeneraUsuario generaUsuario = new GeneraUsuario();
+                generaUsuario.Generar();
+                Persona persona = (Persona)generaUsuario.usuarios[0];
+
+
+                if (txtbUsuario.Text == persona.Usuario && txtbPassword.Text == persona.Password)
+                {
+                    MessageBox.Show("Autenticación exitosa");
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrecta");
+                }
+
+            }
+
+        }
+    }
+}
